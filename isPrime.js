0debc8511f4ac7728849b00ca09d52d4fad89ec4
Tyler Stark <theirondeveloper@gmail.com>
2014-07-19 01:49:47 -0700

    Fixing minor bugs (1 case, and negative number case)

--- isPrime.js
+++ isPrime.js
@@ -6,10 +6,14 @@
 var testNumber = process.argv[2];
 
 /**
- * @param test - an integer between 1 and
+ * @param test - an integer between 1 and 2^16
+ * If the number is negative, we will absolute value it.
+ * We are also assuming 1 is not a prime number.
  * @returns {boolean}
  */
 function isPrime (test) {
+	test = (test^(test>>31))-(test>>31);
+	if (!(test-1)) return false;
 
 	var max = ~~(test/2);
 	while (max>1) {
